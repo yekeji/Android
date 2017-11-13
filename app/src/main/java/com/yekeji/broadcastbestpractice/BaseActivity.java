e6395c6b0e89b50e00986f47462ace6ad250572e
yekeji <983572973@qq.com>
2017-11-13 17:40:04 +0800

    第二提交，广播的最佳实践

--- app/src/main/java/com/yekeji/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/yekeji/broadcastbestpractice/BaseActivity.java
@@ -8,7 +8,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.AlteredCharSequence;
 
 /**
  * Created by ye.keji on 2017-11-13.
--- app/src/main/java/com/yekeji/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/yekeji/broadcastbestpractice/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.yekeji.broadcastbestpractice;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
